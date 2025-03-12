3ffd0a752001a76bb2db875b71603b4c1e9ef59c
alejandro.bello <alejandro.bello@uic.cu>
2025-03-12 18:06:45 -0400

    Category

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,41 @@
+import ProductGrid from "@/components/products/product-grid/ProductGrid";
+import Title from "@/components/ui/title/Title";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+
+const seedProduct = initialData.products;
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
 export default function ({ params }: Props) {
   const { id } = params;
+  const products = seedProduct.filter((product) => product.gender === id);
 
-  if (id === "kids") {
-    notFound();
-  }
+  const labels: Record<Category, string> = {
+    men: "Hombres",
+    women: "Mujeres",
+    kid: "Niños",
+    unisex: "Todos",
+  };
+
+  // if (id === "kids") {
+  //   notFound();
+  // }
 
   return (
-    <div className="">
-      <h1>Category</h1>
-    </div>
+    <>
+      <Title
+        title="Tienda"
+        subtitle={`Artículos para ${labels[id]}`}
+        className="mb-2"
+      />
+
+      <ProductGrid products={products} />
+    </>
   );
 }
